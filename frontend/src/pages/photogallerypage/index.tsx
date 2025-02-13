987598203047675d8f342d954a65dab5a5176a39
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2025-02-13 11:26:49 +0530

    commit build errors

--- frontend/src/pages/photogallerypage/index.tsx
+++ frontend/src/pages/photogallerypage/index.tsx
@@ -1,14 +1,6 @@
-import { imageSliderPhotos } from "../../utiles/image-slider-photos";
 import styles from "../../styles/photogallerypage.module.css";
-import { useState } from "react";
 import ImageSlider from "@/components/ImageSlider";
 
-interface ImageSliderPhoto {
-  id: number;
-  imageUrl: string;
-  name: string;
-  description: string;
-}
 const PhotogalleryPage: React.FC = () => {
   return(
     <div className={styles["container"]}>
