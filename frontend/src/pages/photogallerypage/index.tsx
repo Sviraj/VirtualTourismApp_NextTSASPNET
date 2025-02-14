3092d6a293abc55c4c41e1012338eb461d074aff
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2025-02-14 12:56:52 +0530

    commit changes

--- frontend/src/pages/photogallerypage/index.tsx
+++ frontend/src/pages/photogallerypage/index.tsx
@@ -1,10 +1,29 @@
 import styles from "../../styles/photogallerypage.module.css";
 import ImageSlider from "@/components/ImageSlider";
+import { imageSliderPhotosGallery } from "@/utiles/image-slider-photosGallery";
 
 const PhotogalleryPage: React.FC = () => {
-  return(
+  return (
     <div className={styles["container"]}>
       <ImageSlider />
+
+      <div className={styles["photo-grid"]}>
+        {imageSliderPhotosGallery.map((group, groupIndex) => (
+          <div key={groupIndex}>
+            <div className={styles["photo-card"]}>
+              {group.map((photo) => (
+                <div key={photo.id} className={styles["photo-cardp"]}>
+                  <img src={photo.imageUrl} />
+                </div>
+              ))}
+              <div className={styles["photo-count"]}>3423 photos</div>
+              <div className={styles["background-text"]}>
+                Best arial panoroma photos
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ frontend/src/utiles/image-slider-photosGallery.ts
@@ -0,0 +1,369 @@
+export interface ImageSliderPhoto {
+  id: number;
+  imageUrl: string;
+  name: string;
+  description: string;
+}
+
+export const imageSliderPhotosGallery: ImageSliderPhoto[][] = [
+  [
+    {
+      id: 1,
+      name: "Andaman Islands",
+      description: "  nature stock photos for any print",
+      imageUrl:
+        "https://images.unsplash.com/photo-1618886487325-f665032b6352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+    },
+    {
+      id: 2,
+      name: "Wall of China",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://images.unsplash.com/photo-1591889179105-a3f153522288?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Paradise Islands",
+      description: " The Adobe Stock collection features",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/00/72/13/53/240_F_72135354_bBCoyhEY9OTz84g83vKbHyitNNNP7bQY.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/05/00/13/57/1000_F_500135749_Cl1dMNuH7DpSP7kDaHQxIpeQqo8w5n0E.jpg",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "Andaman Islands",
+      description: "  nature stock photos for any print",
+      imageUrl:
+        "https://images.unsplash.com/photo-1618886487325-f665032b6352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+    },
+    {
+      id: 2,
+      name: "Wall of China",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://images.unsplash.com/photo-1591889179105-a3f153522288?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Paradise Islands",
+      description: " The Adobe Stock collection features",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/00/72/13/53/240_F_72135354_bBCoyhEY9OTz84g83vKbHyitNNNP7bQY.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/05/00/13/57/1000_F_500135749_Cl1dMNuH7DpSP7kDaHQxIpeQqo8w5n0E.jpg",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "Andaman Islands",
+      description: "  nature stock photos for any print",
+      imageUrl:
+        "https://images.unsplash.com/photo-1618886487325-f665032b6352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+    },
+    {
+      id: 2,
+      name: "Wall of China",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://images.unsplash.com/photo-1591889179105-a3f153522288?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Paradise Islands",
+      description: " The Adobe Stock collection features",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/00/72/13/53/240_F_72135354_bBCoyhEY9OTz84g83vKbHyitNNNP7bQY.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/05/00/13/57/1000_F_500135749_Cl1dMNuH7DpSP7kDaHQxIpeQqo8w5n0E.jpg",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Maldives - Paradise Islands, part III",
+      description: "9,454",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/02/86/08/85/1000_F_286088509_GZFeghJcoPjVCLotBR5AgDi3pf1Fjfxp.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert, Argentina. Part I",
+      description: "138,213",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/05/00/13/57/1000_F_500135749_Cl1dMNuH7DpSP7kDaHQxIpeQqo8w5n0E.jpg",
+    },
+    {
+      id: 7,
+      name: "Marathonisi Island, Greece",
+      description: "12,894",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/02/49/82/38/1000_F_249823850_mCxoxfHYPQiQV3DI14QuYSCqYAnfmIdP.jpg",
+    },
+    {
+      id: 8,
+      name: "Maldives Full Moon",
+      description: "13,745",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/02/10/63/94/1000_F_210639467_icLQOnamR82Kjkms824TiF69T3Rb9eih.jpg",
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "Andaman Islands",
+      description: "2,975",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/01/24/53/86/1000_F_124538688_amRnfhQENBJxMOoOfxP9nUaB3p0phHu7.jpg",
+    },
+    {
+      id: 2,
+      name: "Misty morning, Great Wall of China",
+      description: "7,703",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/02/10/63/94/1000_F_210639467_icLQOnamR82Kjkms824TiF69T3Rb9eih.jpg",
+    },
+    {
+      id: 3,
+      name: "The Charyn and the Lunar Canyons in Kazakhstan",
+      description: "4,781",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/02/86/08/85/1000_F_286088509_GZFeghJcoPjVCLotBR5AgDi3pf1Fjfxp.jpg",
+    },
+    {
+      id: 4,
+      name: "Porto Katsiki beach, Lefkada, Greece",
+      description: "11,384",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/02/86/08/85/1000_F_286088509_GZFeghJcoPjVCLotBR5AgDi3pf1Fjfxp.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Maldives - Paradise Islands, part III",
+      description: "9,454",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/02/86/08/85/1000_F_286088509_GZFeghJcoPjVCLotBR5AgDi3pf1Fjfxp.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert, Argentina. Part I",
+      description: "138,213",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/05/00/13/57/1000_F_500135749_Cl1dMNuH7DpSP7kDaHQxIpeQqo8w5n0E.jpg",
+    },
+    {
+      id: 7,
+      name: "Marathonisi Island, Greece",
+      description: "12,894",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/02/49/82/38/1000_F_249823850_mCxoxfHYPQiQV3DI14QuYSCqYAnfmIdP.jpg",
+    },
+    {
+      id: 8,
+      name: "Maldives Full Moon",
+      description: "13,745",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/02/10/63/94/1000_F_210639467_icLQOnamR82Kjkms824TiF69T3Rb9eih.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Maldives - Paradise Islands, part III",
+      description: "9,454",
+      imageUrl:
+        "https://images.unsplash.com/photo-1421789665209-c9b2a435e3dc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZW52aXJvbm1lbnR8ZW58MHx8MHx8fDA%3D",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert, Argentina. Part I",
+      description: "138,213",
+      imageUrl:
+        "https://plus.unsplash.com/premium_photo-1664637952509-c2627f44406b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGVudmlyb25tZW50fGVufDB8fDB8fHww",
+    },
+    {
+      id: 7,
+      name: "Marathonisi Island, Greece",
+      description: "12,894",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGVudmlyb25tZW50fGVufDB8fDB8fHww",
+    },
+    {
+      id: 8,
+      name: "Maldives Full Moon",
+      description: "13,745",
+      imageUrl:
+        "https://media.istockphoto.com/id/1410391090/photo/crystal-globe-putting-on-moss.webp?a=1&b=1&s=612x612&w=0&k=20&c=NromQSeavYXVm9o0uwwx3sMiRjgC5mtTbgQfBXY9I6E=",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Maldives - Paradise Islands, part III",
+      description: "9,454",
+      imageUrl:
+        "https://t3.ftcdn.net/jpg/02/96/26/82/240_F_296268274_ytY0Jo0RFnJip1M2KSE9SXzC4Hw7m9kb.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert, Argentina. Part I",
+      description: "138,213",
+      imageUrl:
+        "https://t3.ftcdn.net/jpg/04/24/62/76/240_F_424627604_HbQA6R6uGhMRYosq1Ddp2owH90cXfJfn.jpg",
+    },
+    {
+      id: 7,
+      name: "Marathonisi Island, Greece",
+      description: "12,894",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/49/13/07/240_F_249130782_R5CGIZxR64uRkV8MhWsub9bexmtgJFXa.jpg",
+    },
+    {
+      id: 8,
+      name: "Maldives Full Moon",
+      description: "13,745",
+      imageUrl:
+        "https://t3.ftcdn.net/jpg/02/19/84/86/240_F_219848647_dRWZu9bw6ejvgkCPfPExycrGOaJeqBwJ.jpg",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Paradise Islands",
+      description: " The Adobe Stock collection features",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/00/72/13/53/240_F_72135354_bBCoyhEY9OTz84g83vKbHyitNNNP7bQY.jpg",
+    },
+    {
+      id: 6,
+      name: "Sahara Desert",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as2.ftcdn.net/v2/jpg/05/00/13/57/1000_F_500135749_Cl1dMNuH7DpSP7kDaHQxIpeQqo8w5n0E.jpg",
+    },
+    {
+      id: 3,
+      name: "Canyons in Kazakhstan",
+      description: "  range of nature stock photos for any print",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/07/01/99/240_F_207019997_b0NsUaexcVMMSheib1Tp8o9Qi08C8daZ.jpg",
+    },
+    {
+      id: 4,
+      name: "Lefkada, Greece",
+      description: "From landscapes and wildlife to close-ups",
+      imageUrl:
+        "https://as1.ftcdn.net/v2/jpg/03/22/88/26/1000_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg.jpg",
+    },
+  ],
+];
